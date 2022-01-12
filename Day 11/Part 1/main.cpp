6b5ee4bf290e025e977841ef3b6fe0674ea186ab
BuildTools <unconfigured@null.spigotmc.org>
2022-01-11 21:20:08 -0800

    Day 11 Parts 1 and 2

---
+++ Day 11/Part 1/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+using namespace std;
+
+ifstream fin("in.txt");
+
+vector<vector<int>> octopuses;
+
+// all directions
+int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dy[8] = {-1, 0, 1, -1, 1, 1, 0, -1};
+long long total = 0;
+
+void update()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            octopuses[i][j]++;
+        }
+    }
+}
+
+void print()
+{
+    for (int i = 0; i < octopuses.size(); i++)
+    {
+        for (int j = 0; j < octopuses[i].size(); j++)
+        {
+            cout << octopuses[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int simFlashes()
+{
+    int flashes = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (octopuses[i][j] > 9)
+            {
+                flashes++;
+                octopuses[i][j] = -10000000;
+                for (int k = 0; k < 8; k++)
+                {
+                    int cx = i + dx[k];
+                    int cy = j + dy[k];
+                    if (cx < 0 || cy < 0 || cx >= 10 || cy >= 10)
+                    {
+                        continue;
+                    }
+                    octopuses[cx][cy]++;
+                }
+            }
+        }
+    }
+    return flashes;
+}
+
+int main()
+{
+    octopuses.assign(10, vector<int>());
+
+    for (int i = 0; i < 10; i++)
+    {
+        string temp;
+        fin >> temp;
+        for (int j = 0; j < temp.size(); j++)
+        {
+            octopuses[i].push_back(temp[j] - '0');
+        }
+    }
+
+    print();
+    cout << endl;
+
+    for (int i = 0; i < 100; i++)
+    {
+        update();
+        int curFlashes = simFlashes();
+        while (curFlashes > 0)
+        {
+            total += curFlashes;
+            curFlashes = simFlashes();
+        }
+        total += curFlashes;
+        for (int j = 0; j < 10; j++)
+        {
+            for (int k = 0; k < 10; k++)
+            {
+                if (octopuses[j][k] < 0)
+                {
+                    octopuses[j][k] = 0;
+                }
+            }
+        }
+        cout << "step " << i + 1 << endl;
+        print();
+    }
+    cout << total << endl;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Note:
+
+This code was created after the competition.
+I created this to practice my skill at solving problems.
+The code has almost no comments, so beware.
+I don't even know what I coded myself.
